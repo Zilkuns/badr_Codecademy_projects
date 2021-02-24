1e91712e9ff235699b96d55d45a1c1ac021bb190
Bader Alrashed <75751751+Zilkuns@users.noreply.github.com>
2021-02-24 14:20:39 +0300

    edit 2

--- Main.py
+++ Main.py
@@ -3,20 +3,18 @@ import csv
 insurances ={}
 columns = []
 line_one = True
-with open("insurance.csv", "w",newline="") as csv_file:
+with open("insurance.csv",newline="") as csv_file:
     reader = csv.reader(csv_file)
     j = 1
     for row in reader:
         internal_dict= {}
         i = 0
-        split_row=row.split(',')
         if line_one:
-            for column in split_row:
+            for column in row:
                 columns.append(column)
             line_one = False
         else:
-            split_row = row.split(",")
-            for value in split_row:
+            for value in row:
                 internal_dict[columns[i]] = value
                 i +=1
         insurances["insurance-"+str(j)] =  internal_dict
